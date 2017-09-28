e712d2e7066cddc7a8d00b421c4b384b7982b714
Radun1 <32276257+Radun1@users.noreply.github.com>
2017-09-29 00:06:09 +0300

    Add files via upload

---
+++ EmployeeData.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+
+namespace EmployeeData
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = Console.ReadLine();
+            int age = int.Parse(Console.ReadLine());
+            int employeeID = int.Parse(Console.ReadLine());
+            decimal monthlySalary = decimal.Parse(Console.ReadLine());
+
+            Console.WriteLine($"Name: {name}");
+            Console.WriteLine($"Age: {age}");
+            Console.WriteLine($"Employee ID: {employeeID:D8}");
+            Console.WriteLine($"Salary: {monthlySalary:f2}");
+        }
+    }
+}
